56800f5d6ecbb6ac435167c90484387d491fbadc
Nikolaj-hvitfeldt <nhvitfeldt1@gmail.com>
2026-04-10 11:30:23 +0200

    feat: Render employees on about page

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,10 +1,23 @@
-import { Text, View, StyleSheet } from "react-native";
+import { employees } from "@/data/employees";
+import { Link } from "expo-router";
+import { Pressable, ScrollView, StyleSheet, Text } from "react-native";
 
 export default function AboutScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>About screen</Text>
-    </View>
+    <ScrollView>
+      <Text>Our Team</Text>
+      <Text>Meet the people behind Sticker Smash</Text>
+
+      {employees.map((employee) => (
+        <Link key={employee.id} href={`/employees/${employee.id}`}>
+          <Pressable>
+            <Text>{employee.name}</Text>
+            <Text>{employee.role}</Text>
+            <Text>{employee.department}</Text>
+          </Pressable>
+        </Link>
+      ))}
+    </ScrollView>
   );
 }
 
