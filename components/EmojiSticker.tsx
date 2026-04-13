83b386b04d2f14c00ce0315e044da41cbe292f62
Nikolaj-hvitfeldt <nhvitfeldt1@gmail.com>
2026-04-13 14:48:58 +0200

    feat: added gyroscope and haptic feedback to EmojiSticker.tsx

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,3 +1,6 @@
+import { impactAsync, ImpactFeedbackStyle } from "expo-haptics";
+import { Gyroscope } from "expo-sensors";
+import { useEffect } from "react";
 import { ImageSourcePropType } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import Animated, {
@@ -5,6 +8,8 @@ import Animated, {
   useSharedValue,
   withSpring,
 } from "react-native-reanimated";
+
+const GYRO_SENS = 0.2;
 
 type Props = {
   imageSize: number;
@@ -15,6 +20,15 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
   const scaleImage = useSharedValue(imageSize);
   const translateX = useSharedValue(0);
   const translateY = useSharedValue(0);
+
+  useEffect(() => {
+    Gyroscope.setUpdateInterval(16);
+    const subscription = Gyroscope.addListener(({ x, y }) => {
+      translateX.value += y * GYRO_SENS;
+      translateY.value += x * GYRO_SENS;
+    });
+    return () => subscription.remove();
+  }, []);
 
   const drag = Gesture.Pan().onChange((event) => {
     translateX.value += event.changeX;
@@ -31,8 +45,10 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
   });
 
   const doubleTap = Gesture.Tap()
+    .runOnJS(true)
     .numberOfTaps(2)
     .onStart(() => {
+      impactAsync(ImpactFeedbackStyle.Medium);
       if (scaleImage.value !== imageSize * 2) {
         scaleImage.value = scaleImage.value * 2;
       } else {
